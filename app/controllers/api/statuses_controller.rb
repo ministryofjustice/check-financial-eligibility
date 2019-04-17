f0ab1396808ac15cc64b136531e3699e091f9e01
willc-work <william.clarke@justice.gov.uk>
2019-04-17 14:12:45 +0100

    Initial commit

---
+++ app/controllers/api/statuses_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  class StatusesController < ApplicationController
+    def index
+      responses = Status.order(created_at: :desc)
+      render json: { status: 'SUCCESS', message: 'Response Messages', data: responses }
+    end
+
+    private
+
+    # def status_params
+    #   params.require(:status).permit(:response)
+    # end
+  end
+end
---
+++ spec/controllers/statuses_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe StatusesController, type: :controller do
+
+end
