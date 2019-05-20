2b0f8f1d9365134ce8caa205dfda57dddacaaee6
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-05-20 16:08:51 +0100

    AP-443 refeactor in line with code review comments

--- spec/services/json_assessment_response_validator_spec.rb
+++ spec/services/json_assessment_response_validator_spec.rb
@@ -5,15 +5,14 @@ describe 'JSON Assessment response' do
     # The response schema references the request schema definitions through an HTTP request, so
     # stub it out here.
     assessment_request_schema = File.read(Rails.root.join('public/schemas/assessment_request.json'))
-    stub_request(:get, "http://localhost:3000/schemas/assessment_request.json").
-      to_return(:body => assessment_request_schema)
+    stub_request(:get, 'http://localhost:3000/schemas/assessment_request.json')
+      .to_return(body: assessment_request_schema)
   end
 
   context 'A valid response' do
     it 'does not create any errors' do
       schema_path = Rails.root.join('public/schemas/assessment_response.json').to_s
-      response_hash = AssessmentResponseFixture.ruby_hash
-      payload = JSON.pretty_generate(response_hash)
+      payload = JSON.pretty_generate(AssessmentResponseFixture.ruby_hash)
       errors = JSON::Validator.fully_validate(schema_path, payload)
       expect(errors).to be_empty
     end
