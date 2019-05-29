406a00f4c2875ba171c2a8a2dfab1140a8e93156
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-05-29 12:14:14 +0100

    AP-621 restructure response object

--- spec/fixtures/assessment_response_fixture.rb
+++ spec/fixtures/assessment_response_fixture.rb
@@ -7,18 +7,57 @@ class AssessmentResponseFixture < BaseAssessmentFixture
       details: {
         passported: true,
         self_employed: false,
-        monthly_gross_income: 2_565.33,
-        upper_income_threshold: 2_567.00,
-        monthly_disposable_income: 220.55,
-        disposable_income_lower_threshold: 310.00,
-        disposable_income_upper_threshold: 733.00,
-        liquid_capital_assessment: 45.00,
-        total_capital_assessment: 2_855.55,
-        total_capital_lower_threshold: 3_000.00,
-        total_capital_upper_threshold: 8_000.00,
-        disposable_capital_assessment: 100.00,
-        monthly_contribution: 0.00,
-        capital_contribution: 0.00
+        income: {
+          monthly_gross_income: 2_565.33,
+          upper_income_threshold: 2_567.00,
+          monthly_disposable_income: 220.55,
+          disposable_income_lower_threshold: 310.00,
+          disposable_income_upper_threshold: 733.00
+        },
+        capital: {
+          liquid_capital_assessment: 45.00,
+          property: {
+            main_dwelling: {
+              pctg_property_deduction: 3.0,
+              net_value_after_deduction: 485_000.0,
+              maximum_mortgage_allowance: 100_000.0,
+              net_value_after_mortgage: 335_000.0,
+              percentage_owned: 60.0,
+              net_equity_value: 201_000.00,
+              property_disregard: 100_000,
+              assessed_capital_value: 101_000
+            },
+            additional_properties: [
+              {
+                pctg_property_deduction: 3.0,
+                net_value_after_deduction: 485_000.0,
+                maximum_mortgage_allowance: 100_000.0,
+                net_value_after_mortgage: 335_000.0,
+                percentage_owned: 60.0,
+                net_equity_value: 201_000.00,
+                property_disregard: 100_000,
+                assessed_capital_value: 101_000
+              },
+              {
+                pctg_property_deduction: 3.0,
+                net_value_after_deduction: 485_000.0,
+                maximum_mortgage_allowance: 100_000.0,
+                net_value_after_mortgage: 335_000.0,
+                percentage_owned: 60.0,
+                net_equity_value: 201_000.00,
+                property_disregard: 100_000,
+                assessed_capital_value: 101_000
+              }
+            ]
+          },
+          total_capital_lower_threshold: 3_000.00,
+          total_capital_upper_threshold: 8_000.00,
+          disposable_capital_assessment: 100.00
+        },
+        contributions: {
+          monthly_contribution: 0.00,
+          capital_contribution: 0.00
+        }
       },
       errors: [
         'This is error message 1',
