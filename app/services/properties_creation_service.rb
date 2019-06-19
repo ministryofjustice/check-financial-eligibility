6e6044e3e6c928834dd46c907fdceb74aaa4f777
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-06-19 14:15:24 +0100

    AP-731 WIP

--- app/services/properties_creation_service.rb
+++ app/services/properties_creation_service.rb
@@ -24,15 +24,15 @@ class PropertiesCreationService
   private
 
   def json_valid?
-    validator = JsonSchemaValidator.new(raw_post, SCHEMA_PATH)
+    validator = JsonSchemaValidator.new(@raw_post, SCHEMA_PATH)
     return true if validator.valid?
     @errors = validator.errors
     false
   end
 
   def assessment_exists?
-    assessment = Assessment.find_by(id: @payload[:assessment_id])
-    if assessment.nil?
+    @assessment = Assessment.find_by(id: @payload[:assessment_id])
+    if @assessment.nil?
       @errors << 'No such assessment id'
       return false
     end
@@ -40,6 +40,53 @@ class PropertiesCreationService
   end
 
   def create_properties
-    
+    new_main_home
+    new_additional_properties
+    return true if @assessment.save
+
+    collect_model_errors
+    false
+  end
+
+  def new_main_home
+    if @payload[:properties][:main_home]
+      new_property(@payload[:properties][:main_home], true)
+    end
+  end
+
+  def new_property(attrs, main_home)
+    attrs[:main_home] = main_home
+    @assessment.properties.new(attrs)
+  end
+
+  def new_additional_properties
+    if @payload[:properties][:additional_properties]
+      @payload[:properties][:additional_properties].each do |attrs|
+        new_property(attrs, false)
+      end
+    end
+  end
+
+  def collect_model_errors
+    @errors = @assessment.errors.full_messages
+    @assessment.properties.each do |property|
+      @errors += property.errors.full_messages
+    end
+  end
+
+  def success_response
+    OpenStruct.new(
+      success: true,
+      objects: @assessment.properties,
+      errors: []
+    )
+  end
+
+  def error_response
+    OpenStruct.new(
+                success: false,
+                objects: nil,
+                errors: @errors
+    )
   end
 end
--- spec/services/properties_creation_service_spec.rb
+++ spec/services/properties_creation_service_spec.rb
@@ -1,40 +1,96 @@
 require 'rails_helper'
 
 RSpec.describe PropertiesCreationService do
+
+  before { stub_call_to_json_schema }
+
   let(:assessment) { create :assessment }
+
   describe '.call' do
-    it 'returns a valid result structure' do
-      result = described_class.call(valid_payload)
-      expect(result.success).to be true
-      expect(result.objects.size).to eq 3
-      expect(result.objects.map(&:class).uniq).to eq [Property]
-      expect(result.errors).to be_empty      
+    context 'valid payload' do
+      it 'returns a valid success response' do
+        result = described_class.call(valid_payload)
+        expect(result.success).to be true
+        expect(result.objects.size).to eq 3
+        expect(result.objects.map(&:class).uniq).to eq [Property]
+        expect(result.errors).to be_empty
+      end
+
+      it 'creates 3 property records for this assessment' do
+        expect {
+          described_class.call(valid_payload)
+        }.to change { assessment.properties.count }.by(3)
+      end
+
+      it 'returns the ids of the new property records in the response' do
+        result = described_class.call(valid_payload)
+        expect(result.objects[0].id).to eq assessment.properties[0].id
+        expect(result.objects[1].id).to eq assessment.properties[1].id
+        expect(result.objects[2].id).to eq assessment.properties[2].id
+      end
     end
+
+    context 'invalid json' do
+      let(:payload)  { invalid_json_payload }
+      it 'returns error response' do
+        response = described_class.call(payload)
+        expect(response.success).to be false
+        expect(response.objects).to be_nil
+        expect(response.errors[0]).to match %r{The property '#/' did not contain a required property of 'assessment_id'}
+        expect(response.errors[1]).to match %r{The property '#/' contains additional properties \["extra_root_attr"\]}
+        expect(response.errors[2]).to match %r{The property '#/properties/main_home' did not contain a required property of 'percentage_owned'}
+        expect(response.errors[3]).to match %r{The property '#/properties/main_home' contains additional properties \["extra_main_home_attr"\]}
+        expect(response.errors[4]).to match %r{The property '#/properties/additional_properties/0' contains additional properties \["extra_main_home_attr"\]}
+      end
+
+      it 'does not create any property records' do
+        expect {
+          described_class.call(payload)
+        }.not_to change { Property.count }
+      end
+    end
+
+  end
+
+  def invalid_json_payload
+    payload = valid_payload_hash.dup
+    payload[:extra_root_attr] = 1
+    payload.delete(:assessment_id)
+    payload[:properties][:main_home][:extra_main_home_attr] = 3
+    payload[:properties][:additional_properties].first[:extra_main_home_attr] = 3
+    payload[:properties][:main_home].delete(:percentage_owned)
+    payload.to_json
+  end
+
+  def valid_payload_hash
+    {
+      assessment_id: assessment.id,
+      properties: {
+        main_home: {
+          value: 500_000,
+          outstanding_mortgage: 200,
+          percentage_owned: 15,
+          shared_with_housing_assoc: true
+        },
+        additional_properties: [
+          {
+            value: 1_000,
+            outstanding_mortgage: 0,
+            percentage_owned: 99,
+            shared_with_housing_assoc: false
+          },
+          {
+            value: 10_000,
+            outstanding_mortgage: 40,
+            percentage_owned: 80,
+            shared_with_housing_assoc: true
+          }
+        ]
+      }
+    }
   end
 
   def valid_payload
-    {
-      assessment_id: assessment.id,
-      main_home: {
-        value: 500_000,
-        outstanding_mortgage: 200,
-        percentage_owned: 15,
-        shared_with_housing_assoc: true
-      },
-      additional_properties: [
-        {
-          value: 1_000,
-          outstanding_mortgage: 0,
-          percentage_owned: 99,
-          shared_with_housing_assoc: false
-        },
-        {
-          value: 10_000,
-          outstanding_mortgage: 40,
-          percentage_owned: 80,
-          shared_with_housing_assoc: true
-        }
-      ]
-    }.to_json
+    valid_payload_hash.to_json
   end
 end
