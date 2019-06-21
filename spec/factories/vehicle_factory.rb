104bd41e48ddac4131dc8976f531aa0b92668507
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-06-21 15:50:20 +0100

    AP-732 tidy up

--- spec/factories/vehicle_factory.rb
+++ spec/factories/vehicle_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :vehicle do
-    assessment { create :assessment }
+    assessment
     value { Faker::Number.decimal(4, 2) }
     loan_amount_outstanding { Faker::Number.decimal(4, 2) }
     date_of_purchase { Faker::Date.between(6.years.ago, 2.months.ago) }
