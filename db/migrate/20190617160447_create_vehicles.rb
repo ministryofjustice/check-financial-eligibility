78363ef51a586f6e675e9456c454c0734eb8c317
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-06-21 10:24:37 +0100

    AP-732 change migration to use t.belongs_to

--- db/migrate/20190617160447_create_vehicles.rb
+++ db/migrate/20190617160447_create_vehicles.rb
@@ -1,7 +1,7 @@
 class CreateVehicles < ActiveRecord::Migration[5.2]
   def change
     create_table :vehicles, id: :uuid do |t|
-      t.uuid :assessment_id
+      t.belongs_to :assessment, foreign_key: true, null: false, type: :uuid
       t.decimal :value
       t.decimal :loan_amount_outstanding
       t.date :date_of_purchase
