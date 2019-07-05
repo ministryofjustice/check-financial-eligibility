b0dc3d5bc09476cc6d2e86ce712ec8c463b058b5
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-07-05 12:23:11 +0100

    AP-766 rebuild schema from migration

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_06_24_151225) do
     t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["assessment_id"], name: "index_benefit_receipts_on_assessment_id"
   end
 
   create_table "dependent_income_receipts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -120,19 +121,22 @@ ActiveRecord::Schema.define(version: 2019_06_24_151225) do
   end
 
   create_table "wage_slips", force: :cascade do |t|
-    t.uuid "assessment_id"
+    t.uuid "assessment_id", null: false
     t.date "payment_date"
     t.decimal "gross_pay"
     t.decimal "paye"
     t.decimal "nic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["assessment_id"], name: "index_wage_slips_on_assessment_id"
   end
 
   add_foreign_key "applicants", "assessments"
   add_foreign_key "bank_accounts", "assessments"
+  add_foreign_key "benefit_receipts", "assessments"
   add_foreign_key "non_liquid_assets", "assessments"
   add_foreign_key "outgoings", "assessments"
   add_foreign_key "properties", "assessments"
   add_foreign_key "vehicles", "assessments"
+  add_foreign_key "wage_slips", "assessments"
 end
