14dbab3b3ce62f10018e535e343ab813d4a73d9b
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-07-22 12:58:04 +0100

    AP-820 Convert DetermineSelfEmployed predicate to use database

--- spec/services/workflow_predicate/determine_self_employed_spec.rb
+++ spec/services/workflow_predicate/determine_self_employed_spec.rb
@@ -1,19 +1,12 @@
 require 'rails_helper'
 
 module WorkflowPredicate
-  RSpec.xdescribe DetermineSelfEmployed do
-    let(:particulars) { double AssessmentParticulars }
+  RSpec.describe DetermineSelfEmployed do
+    let(:assessment) { create :assessment }
     let(:service) { described_class.new(particulars) }
 
     it 'returns false' do
-      # dummy this out until DetermineSelfEmployed acually has code
-      request_hash = {
-        meta_data: {
-          submission_date: Date.today
-        }
-      }
-      request = JSON.parse(request_hash.to_json, object_class: DatedStruct)
-      allow(particulars).to receive(:request).and_return(request)
+      service = described_class.new(assessment)
       expect(service.call).to be false
     end
   end
