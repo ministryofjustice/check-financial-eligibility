b22ec11c1d37cab17c088aafb9a5bb9d2b8ab5f2
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-07-22 14:20:41 +0100

    AP-818 remove unneccessary constants

--- app/services/base_workflow_service.rb
+++ app/services/base_workflow_service.rb
@@ -1,9 +1,6 @@
 class BaseWorkflowService
   attr_accessor :particulars
 
-  REQUEST_METHODS = %i[meta_data applicant applicant_income applicant_outgoings applicant_capital].freeze
-  RESPONSE_METHODS = %i[response_capital response_income response_contributions].freeze
-
   def initialize(assessment)
     @assessment = assessment
     @calculation_period = CalculationPeriod.new(@assessment.submission_date)
