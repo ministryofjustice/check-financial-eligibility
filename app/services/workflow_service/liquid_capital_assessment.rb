5075f218d9a2789f0a4b97b6a5516399d08a1cf8
Mark Whitaker <mark.whitaker@justice.gov.uk>
2019-07-26 12:01:54 +0100

    AP-785 Amend LiquidCapitalAssessment service
    This service has been written to use values from the original large
    JSON payload.  We have now converted the controllers to store values in
    database records, so this service must be refactored to use the
    database records and not the JSON struct.

--- app/services/workflow_service/liquid_capital_assessment.rb
+++ app/services/workflow_service/liquid_capital_assessment.rb
@@ -1,12 +1,12 @@
 module WorkflowService
   class LiquidCapitalAssessment
-    def initialize(request)
-      @request = request
+    def initialize(assessment_id)
+      @assessment = Assessment.find assessment_id
     end
 
     def call
       total_liquid_capital = 0.0
-      @request.bank_accounts.each do |acct|
+      @assessment.bank_accounts.each do |acct|
         total_liquid_capital += acct.lowest_balance if acct.lowest_balance.positive?
       end
       total_liquid_capital.round(2)
