3c5c9893bb377ae466e7e385f422d246c06c669a
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2019-07-31 10:28:59 +0100

    AP-789 Add additional tests for multiple vehicles

--- spec/services/workflow_service/vehicle_assessment_spec.rb
+++ spec/services/workflow_service/vehicle_assessment_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-module WorkflowService
+module WorkflowService # rubocop:disable Metrics/ModuleLength
   RSpec.describe VehicleAssessment do
     let(:assessment) { create :assessment }
     let(:service) { described_class.new(assessment) }
@@ -97,6 +97,47 @@ module WorkflowService
       end
 
       context 'multiple vehicles' do
+        context 'multiple vehicles included not in capital assessment' do
+          let(:vehicle_1) { create :vehicle, value: 9_500, loan_amount_outstanding: 0, date_of_purchase: 26.months.ago.to_date, in_regular_use: true }
+          let(:vehicle_2) { create :vehicle, value: 15_500, loan_amount_outstanding: 0, date_of_purchase: 46.months.ago.to_date, in_regular_use: true }
+          it 'is assessed at zero' do
+            assessment.vehicles << vehicle_1
+            assessment.vehicles << vehicle_2
+            result = service.call
+            expect(result.first).to have_matching_attributes(vehicle_1, common_attributes)
+            expect(result.first.assessed_value).to eq 0.0
+            expect(result[1]).to have_matching_attributes(vehicle_2, common_attributes)
+            expect(result[1].assessed_value).to eq 0.0
+          end
+        end
+
+        context 'multiple vehicles included in capital assessment' do
+          let(:vehicle_1) { create :vehicle, value: 23_700, loan_amount_outstanding: 2_250, date_of_purchase: 15.months.ago.to_date, in_regular_use: true }
+          let(:vehicle_2) { create :vehicle, value: 23_700, loan_amount_outstanding: 2_250, date_of_purchase: 26.months.ago.to_date, in_regular_use: false }
+          it 'is assessed at zero' do
+            assessment.vehicles << vehicle_1
+            assessment.vehicles << vehicle_2
+            result = service.call
+            expect(result.first).to have_matching_attributes(vehicle_1, common_attributes)
+            expect(result.first.assessed_value).to eq 2_160.0
+            expect(result[1]).to have_matching_attributes(vehicle_2, common_attributes)
+            expect(result[1].assessed_value).to eq 23_700.0
+          end
+        end
+
+        context 'mix of vehicles included/not included in capital assessment' do
+          let(:vehicle_1) { create :vehicle, value: 9_500, loan_amount_outstanding: 0, date_of_purchase: 26.months.ago.to_date, in_regular_use: true }
+          let(:vehicle_2) { create :vehicle, value: 23_700, loan_amount_outstanding: 2_250, date_of_purchase: 26.months.ago.to_date, in_regular_use: false }
+          it 'is assessed at zero' do
+            assessment.vehicles << vehicle_1
+            assessment.vehicles << vehicle_2
+            result = service.call
+            expect(result.first).to have_matching_attributes(vehicle_1, common_attributes)
+            expect(result.first.assessed_value).to eq 0.0
+            expect(result[1]).to have_matching_attributes(vehicle_2, common_attributes)
+            expect(result[1].assessed_value).to eq 23_700.0
+          end
+        end
       end
 
       def common_attributes
