006a4851ddb40bc94c98c3985a98b64ce5beefdf
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2019-07-31 10:29:06 +0100

    AP-789 Use Faker for IP addresses
    The Assessment factory uses a sequence to create IP addresses. This
    incremental approach results in failures due to invalid addresses
    when > 255 assessments are created (eg 1.1.1.256).
    
    Change the factory to use Faker::Internet.ip_v4_address instead.

--- spec/factories/assessment_factory.rb
+++ spec/factories/assessment_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :assessment do
     sequence(:client_reference_id) { |n| format('CLIENT-REF-%04d', n) }
-    sequence(:remote_ip) { |n| "192.168.0.#{n}" }
+    remote_ip { Faker::Internet.ip_v4_address }
     submission_date { Date.today }
     matter_proceeding_type { 'domestic_abuse' }
   end
