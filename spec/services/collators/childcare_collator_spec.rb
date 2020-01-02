8d32f68d8f361e6118ea41d3f55dbd570048161a
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-01-02 15:35:31 +0000

    AP-1150 change description  on spec

--- spec/services/collators/childcare_collator_spec.rb
+++ spec/services/collators/childcare_collator_spec.rb
@@ -86,7 +86,7 @@ module Collators
         context 'not employed' do
           context 'in receipt of Student grant' do
             before { create :other_income_source, gross_income_summary: gross_income_summary, name: 'student_loan' }
-            it 'does not update the monthly childcare value on the disposable income summary' do
+            it 'updates the monthly childcare value on the disposable income summary' do
               subject
               expect(disposable_income_summary.monthly_childcare).to eq 155.63
             end
