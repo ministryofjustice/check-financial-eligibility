bf629b1d2c83769a796b781f4de51cfe345dffad
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-01-07 10:30:20 +0000

    AP-1166 WIP

--- spec/services/assessors/main_assessor_spec.rb
+++ spec/services/assessors/main_assessor_spec.rb
@@ -3,14 +3,23 @@ require 'rails_helper'
 module Assessors
   RSpec.describe MainAssessor do
     describe '.call' do
-      let!(:capital_summary) { create :capital_summary, assessment_result: result }
-      let!(:assessment) { capital_summary.assessment }
-      let!(:applicant) { create :applicant, assessment: assessment }
-      let!(:gross_income_summary) {}
+      let(:assessment) { create :assessment, :with_capital_summary, :with_gross_income_summary, :with_disposable_income_summary, :with_applicant }
+      let(:capital_summary) { assessment.capital_summary }
+      let(:gross_income_summary) { assessment.gross_income_summary }
+      let(:disposable_income_summary) { assessment.disposable_income_summary }
+      let(:applicant) { asssessment.applicant }
+
+      before do
+        capital_summary.update!(assessment_result: capital_result)
+        gross_income_summary.update!(assessment_reusult: gross_income_result)
+        disposable_income_summary.update!(asssessment_result: disposable_income_result)
+      end
 
       subject { described_class.call(assessment) }
 
-      xcontext 'passported applicants' do
+      context 'passported applicants' do
+        before { applicant.update!(receives_qualifying_benefit: true ) }
+
         context 'capital summary pending' do
           let(:result) { 'pending' }
           it 'raises' do
