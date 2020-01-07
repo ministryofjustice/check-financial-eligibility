ff68f06a0e5a059e9b131282c1298b1282af6e2b
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-01-07 10:55:40 +0000

    AP-1166 wip

--- spec/services/assessors/main_assessor_spec.rb
+++ spec/services/assessors/main_assessor_spec.rb
@@ -15,42 +15,6 @@ module Assessors
         disposable_income_summary.update!(asssessment_result: disposable_income_result)
       end
 
-      subject { described_class.call(assessment) }
-
-      context 'passported applicants' do
-        before { applicant.update!(receives_qualifying_benefit: true ) }
-
-        context 'capital summary pending' do
-          let(:result) { 'pending' }
-          it 'raises' do
-            expect { subject }.to raise_error RuntimeError, 'Capital assessment not complete'
-          end
-        end
-
-        context 'capital summary eligible' do
-          let(:result) { 'eligible' }
-          it 'raises' do
-            subject
-            expect(assessment.assessment_result).to eq 'eligible'
-          end
-        end
-
-        context 'capital summary not_eligible' do
-          let(:result) { 'not_eligible' }
-          it 'raises' do
-            subject
-            expect(assessment.assessment_result).to eq 'not_eligible'
-          end
-        end
-
-        context 'capital summary contribution_required' do
-          let(:result) { 'contribution_required' }
-          it 'raises' do
-            subject
-            expect(assessment.assessment_result).to eq 'contribution_required'
-          end
-        end
-      end
-    end
+      subject { described_class.call(asfile.
   end
 end
