726ec6e969f8c5292ae0753b0c64c4abdcc8e001
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-01-17 11:51:35 +0000

    Add OutgoingsCollator and DisposableIncomeCalculator

--- app/services/collators/outgoings_collator.rb
+++ app/services/collators/outgoings_collator.rb
@@ -1,7 +1,17 @@
 module Collators
   class OutgoingsCollator < BaseWorkflowService
     def call
-      # placeholder class
+      collate_costs_and_allowances
+      Calculators::DisposableIncomeCalculator.call(assessment)
+    end
+
+    private
+
+    def collate_costs_and_allowances
+      Collators::ChildcareCollator.call(assessment)
+      Collators::DependantsAllowanceCollator.call(assessment)
+      Collators::MaintenanceCollator.call(assessment)
+      Collators::HousingCostsCollator.call(assessment)
     end
   end
 end
