6d75e838152cbf901aa7b77b9bfa453ec9bb44b8
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-02-13 13:36:54 +0000

    Update the housing costs collator
    Expand the tests to cover more scenarios and call the new
    gross_housing_cost method of the calculator

--- app/services/collators/housing_costs_collator.rb
+++ app/services/collators/housing_costs_collator.rb
@@ -4,7 +4,7 @@ module Collators
       housing_calculator = Calculators::HousingCostsCalculator.new(assessment)
       disposable_income_summary.update!(
         housing_benefit: housing_calculator.monthly_housing_benefit,
-        gross_housing_costs: housing_calculator.monthly_actual_housing_costs,
+        gross_housing_costs: housing_calculator.gross_housing_costs,
         net_housing_costs: housing_calculator.net_housing_costs
       )
     end
