44b1b1f70e5af4113f238ddf0e2b33a0a215978a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-02-26 11:36:50 +0000

    Update the CapitalCollator
    Refactor call method, extract the hash to a constant ans
    iterate through it in the call method.
    
    remove the override from rubocop_todo

--- app/services/collators/capital_collator.rb
+++ app/services/collators/capital_collator.rb
@@ -1,19 +1,21 @@
 module Collators
   class CapitalCollator < BaseWorkflowService
+    RETURN_VALUES = {
+      total_liquid: 'liquid_capital',
+      total_non_liquid: 'non_liquid_capital',
+      total_vehicle: 'vehicles',
+      total_mortgage_allowance: 'property_maximum_mortgage_allowance_threshold',
+      total_property: 'property',
+      pensioner_capital_disregard: 'pensioner_capital_disregard',
+      total_capital: 'total_capital',
+      assessed_capital: 'assessed_capital',
+      lower_threshold: 'lower_threshold',
+      upper_threshold: 'upper_threshold',
+      capital_contribution: 'capital_contribution'
+    }.freeze
+
     def call
-      {
-        total_liquid: liquid_capital,
-        total_non_liquid: non_liquid_capital,
-        total_vehicle: vehicles,
-        total_mortgage_allowance: property_maximum_mortgage_allowance_threshold,
-        total_property: property,
-        pensioner_capital_disregard: pensioner_capital_disregard,
-        total_capital: total_capital,
-        assessed_capital: assessed_capital,
-        lower_threshold: lower_threshold,
-        upper_threshold: upper_threshold,
-        capital_contribution: capital_contribution
-      }
+      RETURN_VALUES.deep_transform_values { |value| send(value) }
     end
 
     private
