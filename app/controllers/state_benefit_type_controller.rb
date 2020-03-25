3837a03c9b9d04043c1c69333e04785ae7f87a39
Abdirizak Obsiye <mrabdirizak@gmail.com>
2020-03-25 15:20:06 +0000

    Update apipie documentation

--- app/controllers/state_benefit_type_controller.rb
+++ app/controllers/state_benefit_type_controller.rb
@@ -4,17 +4,12 @@ class StateBenefitTypeController < ApplicationController
     formats ['json']
     description <<-END_OF_TEXT
     == Description
-      Returns all state benefit types.
+      Returns all state benefit types' name, label and dwpcode.
     END_OF_TEXT
   end
 
   api :GET, 'state_benefit_type', 'List of state benefit types'
   formats ['json']
-  param :state_benefit_type, Array, desc: 'Collection of state benefit types' do
-    param :name, String, required: false, desc: 'The state benefit type name'
-    param :label, String, required: false, desc: 'The state benefit type label'
-    param :dwp_code, String, required: false, desc: 'The state benefit type dwpcode'
-  end
 
   def index
     column_names = %w[name label dwp_code]
