def6e7040031337553e18093ea224ac447020783
Abdirizak Obsiye <mrabdirizak@gmail.com>
2020-03-27 12:59:15 +0000

    Update doc and create factory value for dwp_code

--- spec/requests/state_benefit_types_spec.rb
+++ spec/requests/state_benefit_types_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe StateBenefitTypeController, type: :request do
-  let!(:state_benefit_type) { create :state_benefit_type }
-  let(:name) { state_benefit_type.name }
-  let(:label) { state_benefit_type.label }
-  let(:dwp_code) { state_benefit_type.dwp_code }
+  let!(:state_benefit_type_1) { create :state_benefit_type }
+  let!(:state_benefit_type_2) { create :state_benefit_type }
+  let(:name) { state_benefit_type_1.name }
+  let(:label) { state_benefit_type_1.label }
+  let(:dwp_code) { state_benefit_type_1.dwp_code }
 
   describe 'GET state benefit types' do
     subject { get state_benefit_type_index_path }
@@ -12,13 +13,13 @@ RSpec.describe StateBenefitTypeController, type: :request do
       subject
     end
 
-    it 'is successful' do
-      expect(response).to have_http_status(200)
+    it 'returns http success', :show_in_doc do
+      expect(response).to have_http_status(:success)
     end
 
     it 'returns an array of state benefit types' do
       state_benefit_types = { name: name, label: label, dwp_code: dwp_code }.stringify_keys
-      expect(JSON.parse(response.body)).to contain_exactly(state_benefit_types)
+      expect(JSON.parse(response.body).first).to include(state_benefit_types)
     end
   end
 end
