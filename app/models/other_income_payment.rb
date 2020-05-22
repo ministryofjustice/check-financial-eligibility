dfe5438d63624d2977d7d0e56352422cf86a2b6c
Abdirizak Obsiye <mrabdirizak@gmail.com>
2020-05-22 12:53:13 +0100

    Remove DefaultClientId because client_id is now required
    Remove DefaultClientId because client_id is now a required parameter so there'll always be a String value for that

--- app/models/other_income_payment.rb
+++ app/models/other_income_payment.rb
@@ -1,6 +1,4 @@
 class OtherIncomePayment < ApplicationRecord
-  include DefaultClientId
-
   belongs_to :other_income_source
 
   validates :payment_date, :amount, presence: true
--- app/models/outgoings/base_outgoing.rb
+++ app/models/outgoings/base_outgoing.rb
@@ -1,7 +1,5 @@
 module Outgoings
   class BaseOutgoing < ApplicationRecord
-    include DefaultClientId
-
     belongs_to :disposable_income_summary
 
     self.table_name = 'outgoings'
