16a314639c4ee16b60d3128bb1d11a040e38bcdc
Abdirizak Obsiye <mrabdirizak@gmail.com>
2020-06-18 09:38:14 +0100

    Add irregular incomes controller
    Add ApiPie params in this controller for a POST request
    
    Add ApiPie enum validation for VALID_IRREGULAR_INCOME_TYPES and VALID_IRREGULAR_INCOME_FREQUENCIES to make sure
    we're getting the data we expect
    
    Make it use a creation service to do ActiveRecord validations and persist the new data
    
    Use render_success and render_unprocessable methods from the application controller to handle the response

---
+++ app/controllers/irregular_incomes_controller.rb
@@ -0,0 +1,53 @@
+class IrregularIncomesController < ApplicationController
+  VALID_IRREGULAR_INCOME_TYPES = IrregularIncomePayment::VALID_IRREGULAR_INCOME_TYPES.freeze
+  VALID_IRREGULAR_INCOME_FREQUENCIES = IrregularIncomePayment::VALID_IRREGULAR_INCOME_FREQUENCIES.freeze
+
+  resource_description do
+    name 'Irregular income'
+    short 'Add irregular types of income details to an assessment'
+    formats ['json']
+    description <<-END_OF_TEXT
+    == Description
+      Adds details of an applicant's irregular income payments to an assessment.
+    END_OF_TEXT
+  end
+
+  api :POST, 'assessments/:assessment_id/irregular_income', 'Create irregular income'
+  formats ['json']
+  param :assessment_id, :uuid, required: true
+  param :payments, Array, of: Hash, desc: 'Collection of payment types, frequencies and amounts' do
+    param :income_type, VALID_IRREGULAR_INCOME_TYPES, required: true, desc: 'An identifying name for this irregular income payment'
+    param :frequency, VALID_IRREGULAR_INCOME_FREQUENCIES, required: true, desc: 'The frequency of the payment received'
+    param :amount, :currency, required: true, desc: 'Amount of payment'
+  end
+
+  returns code: :ok, desc: 'Successful response' do
+    property :success, ['true'], desc: 'Success flag shows true'
+    property :errors, [], desc: 'Empty array of error messages'
+  end
+  returns code: :unprocessable_entity do
+    property :success, ['false'], desc: 'Success flag shows false'
+    property :errors, array_of: String, desc: 'Description of why object invalid'
+  end
+
+  def create
+    if creation_service.success?
+      render_success
+    else
+      render_unprocessable(creation_service.errors)
+    end
+  end
+
+  private
+
+  def creation_service
+    @creation_service ||= Creators::IrregularIncomeCreator.call(
+      assessment_id: params[:assessment_id],
+      irregular_income: irregular_income_params
+    )
+  end
+
+  def irregular_income_params
+    JSON.parse(request.raw_post, symbolize_names: true)
+  end
+end
