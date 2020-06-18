49c995911e861bebad69decb48aaa2eef656d4a8
Abdirizak Obsiye <mrabdirizak@gmail.com>
2020-06-18 10:17:05 +0100

    Update MonthlyIncomeEquivalentDecorator and add a test
    Update MonthlyIncomeEquivalentDecorator to only include student_loan as a key in the response when there is no irregular_income (which will handle student loan payments).
    
    Add a test for the MonthlyIncomeEquivalentDecorator

--- app/services/decorators/monthly_income_equivalent_decorator.rb
+++ app/services/decorators/monthly_income_equivalent_decorator.rb
@@ -9,8 +9,18 @@ module Decorators
         friends_or_family: @record.friends_or_family,
         maintenance_in: @record.maintenance_in,
         property_or_lodger: @record.property_or_lodger,
-        student_loan: @record.student_loan,
         pension: @record.pension
+      }.merge(student_loan)
+    end
+
+    private
+
+    # TODO: remove student loan when irregular income handles it
+    def student_loan
+      return {} if @record.irregular_income_payments.present?
+
+      {
+        student_loan: @record.student_loan
       }
     end
   end
