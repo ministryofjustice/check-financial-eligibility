66d72de4250305a4fa769b6f328730acd47c7a36
Abdirizak Obsiye <mrabdirizak@gmail.com>
2020-06-18 10:10:48 +0100

    Update GrossIncomeSummaryDecorator and its test
    Update GrossIncomeSummaryDecorator to include irregular_income into the json response
    
    Only include monthly_student_loan key when student loan is not in the other income sources. This prevents confusion with monthly_student_loan
    and the student_loan in the other income. Also, this will be updated in the future to no longer use student_loan in other income and thus adding the key monthly_student_loan
    into the json response.
    
    Update the decorator tests.

--- app/services/decorators/gross_income_summary_decorator.rb
+++ app/services/decorators/gross_income_summary_decorator.rb
@@ -12,16 +12,33 @@ module Decorators
     def as_json # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
       return nil if @record.nil?
 
+      monthly_student_loan.merge(
+        {
+          monthly_other_income: @record.monthly_other_income,
+          monthly_state_benefits: @record.monthly_state_benefits,
+          total_gross_income: @record.total_gross_income,
+          upper_threshold: @record.upper_threshold,
+          assessment_result: @record.assessment_result,
+          monthly_income_equivalents: MonthlyIncomeEquivalentDecorator.new(@record).as_json,
+          monthly_outgoing_equivalents: MonthlyOutgoingEquivalentDecorator.new(disposable_income_summary).as_json,
+          state_benefits: @record.state_benefits.map { |sb| StateBenefitDecorator.new(sb).as_json },
+          other_income: @record.other_income_sources.map { |oi| OtherIncomeSourceDecorator.new(oi).as_json },
+          irregular_income: IrregularIncomePaymentsDecorator.new(@record.irregular_income_payments).as_json
+        }
+      )
+    end
+
+    private
+
+    # TODO: remove this method and add monthly_student_loan key/value to the above json response when other income no longer includes student loan
+    def monthly_student_loan
+      @record.other_income_sources.each do |source|
+        return {} if source.student_payment?
+      end
+      return {} unless @record.irregular_income_payments.present?
+
       {
-        monthly_other_income: @record.monthly_other_income,
-        monthly_state_benefits: @record.monthly_state_benefits,
-        total_gross_income: @record.total_gross_income,
-        upper_threshold: @record.upper_threshold,
-        assessment_result: @record.assessment_result,
-        monthly_income_equivalents: MonthlyIncomeEquivalentDecorator.new(@record).as_json,
-        monthly_outgoing_equivalents: MonthlyOutgoingEquivalentDecorator.new(disposable_income_summary).as_json,
-        state_benefits: @record.state_benefits.map { |sb| StateBenefitDecorator.new(sb).as_json },
-        other_income: @record.other_income_sources.map { |oi| OtherIncomeSourceDecorator.new(oi).as_json }
+        monthly_student_loan: @record.monthly_student_loan
       }
     end
   end
