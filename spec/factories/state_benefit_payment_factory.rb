1a7afe69aafa10c8548299eda197eedcc74c5643
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-10-26 11:27:55 +0000

    Update the StateBenefitPayment factory

--- spec/factories/state_benefit_payment_factory.rb
+++ spec/factories/state_benefit_payment_factory.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
 
     payment_date { Date.today }
     amount { 123.45 }
+    flags { nil }
+
+    trait :with_multi_benefit_flag do
+      flags { ['multi_benefit'] }
+    end
   end
 end
