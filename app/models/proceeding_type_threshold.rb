f90aea04f14e6908b92d43525e0f99bc657ae3a3
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-04-09 15:37:27 +0100

    AP-2175 Provide data to return correct thrshold values for a given proceeding type

---
+++ app/models/proceeding_type_threshold.rb
@@ -0,0 +1,104 @@
+# This class returns the correct threshold for a specified proceeding type
+#
+class ProceedingTypeThreshold
+  WAIVABLE_THRESHOLDS = %i[capital_upper gross_income_upper disposable_income_upper].freeze
+
+  WAIVERS = {
+    DA001: {
+      capital_upper: true,
+      gross_income_upper: true,
+      disposable_income_upper: true
+    },
+    DA002: {
+      capital_upper: true,
+      gross_income_upper: true,
+      disposable_income_upper: true
+    },
+    DA003: {
+      capital_upper: true,
+      gross_income_upper: true,
+      disposable_income_upper: true
+    },
+    DA004: {
+      capital_upper: true,
+      gross_income_upper: true,
+      disposable_income_upper: true
+    },
+    DA005: {
+      capital_upper: true,
+      gross_income_upper: true,
+      disposable_income_upper: true
+    },
+    DA006: {
+      capital_upper: true,
+      gross_income_upper: true,
+      disposable_income_upper: true
+    },
+    DA007: {
+      capital_upper: true,
+      gross_income_upper: true,
+      disposable_income_upper: true
+    },
+    DA020: {
+      capital_upper: true,
+      gross_income_upper: true,
+      disposable_income_upper: true
+    },
+    SE003: {
+      capital_upper: false,
+      gross_income_upper: false,
+      disposable_income_upper: false
+    },
+    SE004: {
+      capital_upper: false,
+      gross_income_upper: false,
+      disposable_income_upper: false
+    },
+    SE013: {
+      capital_upper: false,
+      gross_income_upper: false,
+      disposable_income_upper: false
+    },
+    SE014: {
+      capital_upper: false,
+      gross_income_upper: false,
+      disposable_income_upper: false
+    }
+  }.freeze
+
+  def self.value_for(ccms_code, threshold, at)
+    new(ccms_code, threshold, at).value
+  end
+
+  def initialize(ccms_code, threshold, at)
+    @ccms_code = ccms_code
+    @threshold = threshold
+    @at = at
+  end
+
+  def value
+    waivable_threshold? ? waivable_value : standard_value
+  end
+
+  private
+
+  def waivable_threshold?
+    @threshold.in?(WAIVABLE_THRESHOLDS)
+  end
+
+  def standard_value
+    Threshold.value_for(@threshold, at: @at)
+  end
+
+  def waivable_value
+    waived? ? waived_value : standard_value
+  end
+
+  def waived_value
+    Threshold.value_for(:infinite_gross_income_upper, at: @at)
+  end
+
+  def waived?
+    WAIVERS.fetch(@ccms_code).fetch(@threshold)
+  end
+end
