4eec5b89e5a93ab4f9e40aaef2fda05ce03f61b4
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-04-26 10:25:27 +0100

    AP-2176 remove unused specs

--- spec/models/gross_income_summary_spec.rb
+++ spec/models/gross_income_summary_spec.rb
@@ -36,26 +36,4 @@ RSpec.describe GrossIncomeSummary do
       end
     end
   end
-
-  # describe '#summarise!' do
-  #   let(:data) do
-  #     {
-  #       upper_threshold: Faker::Number.decimal
-  #     }
-  #   end
-  #
-  #   subject { gross_income_summary.summarise! }
-  #
-  #   before do
-  #     allow(Collators::GrossIncomeCollator).to receive(:call).with(assessment).and_return(data)
-  #     subject
-  #     gross_income_summary.reload
-  #   end
-  #
-  #   it 'persists the data' do
-  #     data.each do |method, value|
-  #       expect(gross_income_summary.__send__(method).to_d).to eq(value.to_d)
-  #     end
-  #   end
-  # end
 end
