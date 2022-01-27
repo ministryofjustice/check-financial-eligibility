2171c445c44641defd1753eef7f7bdc6ba0912ca
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2022-01-27 16:41:15 +0000

    fix cop RSpec/FactoryBot/FactoryClassName

--- spec/factories/eligibility_factory.rb
+++ spec/factories/eligibility_factory.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :capital_eligibility, class: Eligibility::Capital do
+  factory :capital_eligibility, class: 'Eligibility::Capital' do
     capital_summary
     proceeding_type_code { 'DA001' }
     lower_threshold { 3_000.0 }
@@ -7,14 +7,14 @@ FactoryBot.define do
     assessment_result { 'pending' }
   end
 
-  factory :gross_income_eligibility, class: Eligibility::GrossIncome do
+  factory :gross_income_eligibility, class: 'Eligibility::GrossIncome' do
     gross_income_summary
     proceeding_type_code { 'DA001' }
     upper_threshold { 999_999_999_999.0 }
     assessment_result { 'pending' }
   end
 
-  factory :disposable_income_eligibility, class: Eligibility::DisposableIncome do
+  factory :disposable_income_eligibility, class: 'Eligibility::DisposableIncome' do
     disposable_income_summary
     proceeding_type_code { 'DA001' }
     lower_threshold { 315.0 }
@@ -22,7 +22,7 @@ FactoryBot.define do
     assessment_result { 'pending' }
   end
 
-  factory :assessment_eligibility, class: Eligibility::Assessment do
+  factory :assessment_eligibility, class: 'Eligibility::Assessment' do
     assessment
     proceeding_type_code { 'DA001' }
     lower_threshold { nil }
