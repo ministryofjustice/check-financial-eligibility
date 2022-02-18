2bfa19aa3081385dda5ecbbf9928aea2898f7c80
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2022-02-18 17:53:17 +0000

    AP-2892 EmploymentIncomeVariationChecker v2

--- spec/services/utilities/employment_income_variation_checker_spec.rb
+++ spec/services/utilities/employment_income_variation_checker_spec.rb
@@ -1,8 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 module Utilities
   RSpec.describe EmploymentIncomeVariationChecker do
     let(:employment) { create :employment }
+    let(:result) { described_class.new(employment).below_threshold? }
 
     before do
       amounts.each do |amount|
@@ -10,19 +11,35 @@ module Utilities
       end
     end
 
-    subject(:service_call) { described_class.call(employment) }
+    context "no variance" do
+      let(:amounts) { [2000.0, 2000.0, 2000.0, 2000.0] }
 
-    context 'no variance' do
-      let(:amounts) { [ 2000.0, 2000.0, 2000.0, 2000.0] }
-      it 'returns zero' do
-        expect(service_call).to be_zero
+      it "is true" do
+        expect(result).to be true
       end
     end
 
-    context 'variances' do
-      let(:amounts) { [ 879.33, 899.22, 733.22, 801.0, 902.15] }
-      it 'returns difference between highest and lowest' do
-        expect(service_call).to eq 168.93
+    context "variance less than £60" do
+      let(:amounts) { [2000.0, 1941.0, 1966.0, 1996.0] }
+
+      it "is  true" do
+        expect(result).to be true
+      end
+    end
+
+    context "variance exactly £60" do
+      let(:amounts) { [2000.0, 1940.0, 1966.0, 1996.0] }
+
+      it "is false" do
+        expect(result).to be false
+      end
+    end
+
+    context "variance greater £60" do
+      let(:amounts) { [2000.0, 1922.0, 1966.0, 1996.0] }
+
+      it "is false" do
+        expect(result).to be false
       end
     end
   end
