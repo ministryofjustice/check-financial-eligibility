26baffc9cbb2fe0c3af224d391069ce1b0a634fe
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-02-23 07:43:40 +0000

    AP-2784: Update the application config
    Set it to 7, bypassing 6 completely!

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,8 @@ Bundler.require(*Rails.groups)
 module CheckFinancialEligibility
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
+    config.load_defaults 7.0
+    config.active_record.legacy_connection_handling = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
