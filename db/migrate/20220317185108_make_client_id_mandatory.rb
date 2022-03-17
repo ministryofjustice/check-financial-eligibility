1b9c4d82278948868210a3dda9894f0188eb1de8
willc-work <william.clarke@digital.justice.gov.uk>
2022-03-17 19:14:12 +0000

    make value mandatory in DB

---
+++ db/migrate/20220317185108_make_client_id_mandatory.rb
@@ -0,0 +1,15 @@
+class MakeClientIdMandatory < ActiveRecord::Migration[7.0]
+  def up
+    Employment.where(client_id: nil).update_all(client_id: 'not-specified')
+    EmploymentPayment.where(client_id: nil).update_all(client_id: 'not-specified')
+    change_column :employment_payments, :client_id, :string, null: false
+    change_column :employments, :client_id, :string, null: false
+  end
+
+  def down
+    Employment.where(client_id: 'not-specified').update_all(client_id: nil)
+    EmploymentPayment.where(client_id: 'not-specified').update_all(client_id: nil)
+    change_column :employment_payments, :client_id, :string, null: true
+    change_column :employments, :client_id, :string, null: true
+  end
+end
