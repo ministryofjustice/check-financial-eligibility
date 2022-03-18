bf30b59cf7f3ce5ef07f84ac3a07d14e2f8e2409
willc-work <william.clarke@digital.justice.gov.uk>
2022-03-18 12:56:49 +0000

    rubocop

--- db/migrate/20220317185108_make_client_id_mandatory.rb
+++ db/migrate/20220317185108_make_client_id_mandatory.rb
@@ -1,14 +1,14 @@
 class MakeClientIdMandatory < ActiveRecord::Migration[7.0]
   def up
-    Employment.where(client_id: nil).update_all(client_id: 'not-specified')
-    EmploymentPayment.where(client_id: nil).update_all(client_id: 'not-specified')
+    Employment.where(client_id: nil).update_all(client_id: "not-specified")
+    EmploymentPayment.where(client_id: nil).update_all(client_id: "not-specified")
     change_column :employment_payments, :client_id, :string, null: false
     change_column :employments, :client_id, :string, null: false
   end
 
   def down
-    Employment.where(client_id: 'not-specified').update_all(client_id: nil)
-    EmploymentPayment.where(client_id: 'not-specified').update_all(client_id: nil)
+    Employment.where(client_id: "not-specified").update_all(client_id: nil)
+    EmploymentPayment.where(client_id: "not-specified").update_all(client_id: nil)
     change_column :employment_payments, :client_id, :string, null: true
     change_column :employments, :client_id, :string, null: true
   end
