1733240e4ee9398f56335e622db7b5d9ba0259e8
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-05-13 14:20:54 +0100

    Add v3 and v4 assessment swagger docs
    V3 and V4 of assessments endpoint for
    creation function slightly differently.
    
    V3 requires a matter type while v4 does
    not, instead requiring a proceeding type
    CCMS codes array.

---
+++ spec/requests/swagger_docs/v3/assessments_spec.rb
@@ -0,0 +1,100 @@
+require "swagger_helper"
+
+RSpec.describe "V3 Assessments", type: :request, vcr: true, swagger_doc: "v3/swagger.yaml" do
+  path "/assessments" do
+    post("create assessment") do
+      tags "Assessment"
+      consumes "application/json"
+      produces "application/json"
+
+      parameter name: "Accept",
+                in: :header,
+                schema: {
+                  type: :string,
+                  description: "Accept Header with appended version",
+                  example: "application/json;version=3",
+                }
+
+      parameter name: :params,
+                in: :body,
+                required: true,
+                schema: {
+                  type: :object,
+                  properties: {
+                    client_reference_id: {
+                      type: :string,
+                      example: "LA-FOO-BAR",
+                      description: "Client's reference number for this application (free text)",
+                    },
+                    submission_date: {
+                      type: :string,
+                      description: "Date of the original submission (iso8601 format)",
+                      example: Time.zone.now.to_date.iso8601,
+                    },
+                    matter_proceeding_type: {
+                      type: :string,
+                      description: "Matter type of the case (v3 and below only)",
+                      example: Assessment.matter_proceeding_types.values.first,
+                    },
+                  },
+                }
+
+      # rubocop:disable RSpec/VariableName
+      let(:Accept) { "application/json;version=3" }
+      # rubocop:enable RSpec/VariableName
+
+      response(200, "successful") do
+        let(:params) do
+          {
+            submission_date: Time.zone.now.to_date.iso8601,
+            matter_proceeding_type: Assessment.matter_proceeding_types.values.first,
+          }
+        end
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            "application/json" => {
+              example: JSON.parse(response.body, symbolize_names: true),
+            },
+          }
+        end
+
+        run_test!
+      end
+
+      response(422, "Unprocessable Entity") do
+        context ""
+        let(:params) { {} }
+
+        run_test! do |response|
+          body = JSON.parse(response.body, symbolize_names: true)
+          expect(body[:errors]).to include("Missing parameter submission_date")
+        end
+      end
+    end
+  end
+
+  path "/assessments/{id}" do
+    parameter name: :id, in: :path, type: :string, description: "Unique identifier of the assessment"
+
+    get("show assessment") do
+      tags "Assessment"
+      produces "application/json"
+
+      response(200, "successful") do
+        let(:assessment) { create(:assessment, :passported, :with_everything, :with_eligibilities) }
+        let(:id) { assessment.id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            "application/json" => {
+              example: JSON.parse(response.body, symbolize_names: true),
+            },
+          }
+        end
+
+        run_test!
+      end
+    end
+  end
+end
--- spec/requests/swagger_docs/v4/applicants_spec.rb
+++ spec/requests/swagger_docs/v4/applicants_spec.rb
@@ -4,35 +4,51 @@ require "swagger_helper"
 RSpec.describe "applicants", type: :request, swagger_doc: "v4/swagger.yaml" do
   path "/assessments/{assessment_id}/applicant" do
     post("create applicant") do
-      tags "Applicants"
+      tags "Assessment"
       consumes "application/json"
       produces "application/json"
-      parameter name: "assessment_id", in: :path, type: :uuid, description: "assessment_id"
-      parameter name: "applicant",
+
+      parameter name: "assessment_id",
+                in: :path,
+                type: :string,
+                description: <<~DESC
+                  Assessment id to which this applicant relates.
+
+                  This must have been created prior to this call using POST /assessments endpoint
+                DESC
+
+      parameter name: :params,
                 in: :body,
                 required: true,
                 schema: {
                   type: :object,
                   properties: {
-                    date_of_birth: { type: :date,
-                                     example: "1992-07-22",
-                                     description: "Applicant date of birth" },
-                    involvement_type: { type: :string,
-                                        example: "applicant",
-                                        description: "Applicant involvement type" },
-                    has_partner_opponent: { type: :boolean,
-                                            example: false,
-                                            description: "Applicant has partner opponent" },
-                    receives_qualifying_benefit: { type: :boolean,
-                                                   example: false,
-                                                   description: "Applicant receives qualifying benefit" },
+                    applicant: {
+                      type: :object,
+                      description: "Object describing pertinent applicant details",
+                      properties: {
+                        date_of_birth: { type: :date,
+                                         example: "1992-07-22",
+                                         description: "Applicant date of birth" },
+                        involvement_type: { type: :string,
+                                            example: "applicant",
+                                            description: "Applicant involvement type" },
+                        has_partner_opponent: { type: :boolean,
+                                                example: false,
+                                                description: "Applicant has partner opponent" },
+                        receives_qualifying_benefit: { type: :boolean,
+                                                       example: false,
+                                                       description: "Applicant receives qualifying benefit" },
+                      },
+                    },
                   },
                 }
 
       response(200, "successful") do
-        let!(:assessment) { create :assessment }
+        let(:assessment) { create :assessment }
         let(:assessment_id) { assessment.id }
-        let(:applicant) do
+
+        let(:params) do
           {
             applicant: {
               date_of_birth: "1992-07-22",
