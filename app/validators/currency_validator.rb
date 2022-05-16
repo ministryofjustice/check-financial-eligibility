8c9f3d2697362479923ee49d892b2627c4307518
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2022-05-16 14:30:55 +0100

    ap-3113: refactor currency_validator wip

--- app/validators/currency_validator.rb
+++ app/validators/currency_validator.rb
@@ -1,23 +1,15 @@
-class CurrencyValidator < Apipie::Validator::BaseValidator
+class CurrencyValidator < ActiveModel::Validations::NumericalityValidator
   STANDARD_REGEX =  /\A^[-+]?\d+(\.\d{1,2})?\Z$/
   NO_NEGATIVE_REGEX = /\A^[+]?\d+(\.\d{1,2})?\Z$/
 
-  def self.build(param_description, argument, options, _block)
-    new(param_description, options[:currency_option]) if argument == :currency
+  def validate_each(record, attr_name, value)
+    not_negative = options[:not_negative]
+    regex = not_negative == true ? NO_NEGATIVE_REGEX : STANDARD_REGEX
+    record.errors.add(attr_name, description(not_negative)) unless regex.match?(value.to_s)
   end
 
-  def initialize(param_description, option = nil)
-    super(param_description)
-    @option = option
-  end
-
-  def validate(value)
-    regex = @option == :not_negative ? NO_NEGATIVE_REGEX : STANDARD_REGEX
-    regex.match?(value.to_s)
-  end
-
-  def description
-    if @option == :not_negative
+  def description(not_negative)
+    if not_negative == true
       "Must be a decimal, zero or greater, with a maximum of two decimal places. For example: 123.34"
     else
       "Must be a decimal with a maximum of two decimal places. For example: 123.34"
--- spec/validators/currency_validator_spec.rb
+++ spec/validators/currency_validator_spec.rb
@@ -1,77 +1,80 @@
 require "rails_helper"
 
-RSpec.describe CurrencyValidator do
-  let(:params_description) { Faker::Commerce.product_name.underscore }
-  let(:option) { nil }
+RSpec.describe CurrencyValidator, type: :validator do
+  subject(:foo_instance) { foo_model.new(currency_field: currency_value) }
 
-  subject(:currency_validator) { described_class.new(params_description, option) }
+  let(:foo_model) do
+    Class.new do
+      include ActiveModel::Model
+      # include ActiveModel::Validations
 
-  describe "#validate" do
-    context "no date option" do
-      let(:option) { nil }
-
-      context "valid positive and negative numbers" do
-        it "returns true with a valid input" do
-          [10, 10_000, 123.1, 123.12, -33.55].each do |number|
-            expect(currency_validator.validate(number)).to be true
-          end
-        end
+      def self.name
+        "FooModel"
       end
 
-      context "invalid input" do
-        it "returns false" do
-          ["foo", 1.123444].each do |input|
-            expect(currency_validator.validate(input)).to be false
-          end
-        end
-      end
-    end
+      attr_accessor :currency_field
 
-    context "option :not_negative" do
-      let(:option) { :not_negative }
-
-      context "value is negative" do
-        it "is invalid" do
-          expect(currency_validator.validate(-33.44)).to be false
-        end
-      end
-
-      context "value is zero" do
-        it "is valid" do
-          expect(currency_validator.validate(0.00)).to be true
-        end
-      end
-
-      context "value is positive" do
-        it "is valid" do
-          expect(currency_validator.validate(250.20)).to be true
-        end
-      end
-
-      context "invalid input" do
-        it "returns false" do
-          ["foo", 1.123444].each do |input|
-            expect(currency_validator.validate(input)).to be false
-          end
-        end
-      end
+      validates :currency_field, currency: { greater_than_or_equal_to: 0 }, allow_blank: true
     end
   end
 
-  describe "description" do
-    context "no currency option" do
-      let(:option) { nil }
+  context "with integer values" do
+    let(:currency_value) { 10 }
 
-      it "returns a general message" do
-        expect(currency_validator.description).to eq "Must be a decimal with a maximum of two decimal places. For example: 123.34"
+    it "is valid" do
+      expect(foo_instance).to be_valid
+      expect(foo_instance.errors[:currency_field]).to eq []
+    end
+  end
+
+  context "with string values" do
+    let(:currency_value) { "foo" }
+
+    it "is invalid" do
+      expect(foo_instance).to be_invalid
+      expect(foo_instance.errors[:currency_field]).to include("Must be a decimal with a maximum of two decimal places. For example: 123.34")
+    end
+  end
+
+  context "with negative values" do
+    let(:currency_value) { -33.55 }
+
+    it "is valid" do
+      expect(foo_instance).to be_valid
+      expect(foo_instance.errors[:currency_field]).to eq []
+    end
+  end
+
+  context "with not_negative option" do
+    let(:foo_model) do
+      Class.new do
+        include ActiveModel::Model
+
+        def self.name
+          "FooModel"
+        end
+
+        attr_accessor :currency_field
+
+        validates :currency_field, currency: { greater_than_or_equal_to: 0, not_negative: true }, allow_blank: true
       end
     end
 
-    context "currency option :not_negative" do
-      let(:option) { :not_negative }
+    context "with negative values" do
+      let(:currency_value) { -10 }
 
-      it "returns a general message" do
-        expect(currency_validator.description).to eq "Must be a decimal, zero or greater, with a maximum of two decimal places. For example: 123.34"
+      it "is invalid" do
+        expect(foo_instance).to be_invalid
+        expect(foo_instance.errors[:currency_field]).to include("Must be a decimal, zero or greater, with a maximum of two decimal places. For example: 123.34")
+      end
+    end
+
+    context "with positive values" do
+      let(:currency_value) { 10 }
+
+      it "is valid" do
+        expect(foo_instance).to be_valid
+        expect(foo_instance.errors[:currency_field]).to eq []
       end
     end
   end
