173fba16b8bbf72aca5a31aa41f313e8871e33d5
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-05-18 07:01:39 +0100

    Add vehicles swagger docs

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,12 +2,7 @@ class VehiclesController < ApplicationController
   api :POST, "assessments/:assessment_id/vehicles", "Create vehicles"
   formats(%w[json])
   param :assessment_id, :uuid, required: true
-  param(
-    :vehicles,
-    Array,
-    desc: "An array of vehicles owned by the applicant",
-    required: true,
-  ) do
+  param(:vehicles, Array, required: true, desc: "An array of vehicles owned by the applicant") do
     param :value, :currency, currency_option: :not_negative, required: true, desc: "Value of the vehicle"
     param :loan_amount_outstanding, :currency, required: false, desc: "Amount, if any, of a loan used to purchase the vehicle which is still left to pay"
     param :date_of_purchase, Date, date_option: :today_or_older, required: true, desc: "Date the vehicle was purchased by the applicant"
