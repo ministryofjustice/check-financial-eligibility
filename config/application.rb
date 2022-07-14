c33f5c897d34b0cb34a9e4ffbd87fe0144afb651
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-07-14 12:26:05 +0100

    Fix: Add yaml_column_permitted_classes config setting
    Changes in rails 7.0.3.1 prevented Synbols being used in serialised fields
    this overrides the setting and allows the code(and tests) to run as normal
    
    Remarks has to be `required` before it can be added to the array

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require_relative "../app/services/remarks"
 
 module CheckFinancialEligibility
   class Application < Rails::Application
@@ -31,6 +32,10 @@ module CheckFinancialEligibility
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Changes in rails 7.0.3.1 prevented Symbols being used in serialised fields
+    # this overrides the setting and allows the code(and tests) to run as normal
+    config.active_record.yaml_column_permitted_classes = [Symbol, Date, Remarks]
 
     config.x.application.allow_future_submission_date = ENV["ALLOW_FUTURE_SUBMISSION_DATE"] || false
     config.x.status.build_date = ENV["BUILD_DATE"] || "Not Available"
