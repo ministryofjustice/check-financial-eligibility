0620f9d54dde8bff9e9f1d8802295f41ab8aa9ed
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-09-02 17:09:15 +0100

    Add regular transaction creator service

---
+++ app/services/creators/regular_transactions_creator.rb
@@ -0,0 +1,55 @@
+module Creators
+  class RegularTransactionsCreator < BaseCreator
+    attr_accessor :assessment_id
+
+    delegate :gross_income_summary, to: :assessment
+
+    def initialize(assessment_id:, regular_transaction_params: [])
+      super()
+      @assessment_id = assessment_id
+      @regular_transaction_params = regular_transaction_params
+    end
+
+    def call
+      if json_validator.valid?
+        create_records
+      else
+        errors.concat(json_validator.errors)
+      end
+
+      self
+    end
+
+  private
+
+    def create_records
+      ActiveRecord::Base.transaction do
+        assessment
+        create_regular_transaction
+      rescue CreationError => e
+        self.errors = e.errors
+      end
+    end
+
+    def create_regular_transaction
+      return if regular_transactions.empty?
+
+      regular_transactions.each do |regular_transaction|
+        gross_income_summary.regular_transactions.create!(
+          category: regular_transaction[:category],
+          operation: regular_transaction[:operation],
+          amount: regular_transaction[:amount],
+          frequency: regular_transaction[:frequency],
+        )
+      end
+    end
+
+    def regular_transactions
+      @regular_transaction_params[:regular_transactions]
+    end
+
+    def json_validator
+      @json_validator ||= JsonValidator.new("regular_transactions", @regular_transaction_params)
+    end
+  end
+end
