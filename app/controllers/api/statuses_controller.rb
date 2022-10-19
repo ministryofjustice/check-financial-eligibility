b7108abae4442f3102d2502ed0afaabe90b6cd2f
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2022-10-19 09:29:22 +0100

    Remove code that I believe is redundant
    and was causing coverage to fail

--- app/controllers/api/statuses_controller.rb
+++
@@ -1,8 +0,0 @@
-module API
-  class StatusesController < ApplicationController
-    def index
-      responses = Status.order(created_at: :desc)
-      render json: { status: "SUCCESS", message: "Response Messages", data: responses }
-    end
-  end
-end
