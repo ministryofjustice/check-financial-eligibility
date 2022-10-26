a910ec0179cd2edb47d9531b4493f1cf1e303e67
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2022-10-26 17:01:44 +0100

    Update URLs in cucumber script files

--- features/scripts/mac/raise_pull_request.sh
+++ features/scripts/mac/raise_pull_request.sh
@@ -4,4 +4,4 @@ cd $CUCUMBER_REPO_PATH
 
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-open "https://github.com/forceedge01/check-financial-eligibility/compare/main...forceedge01:check-financial-eligibility:${CURRENT_BRANCH}?expand=1"
+open "https://github.com/ministryofjustice/check-financial-eligibility/compare/main...ministryofjustice:check-financial-eligibility:${CURRENT_BRANCH}?expand=1"
--- features/scripts/windows/open_test_runner.bat
+++ features/scripts/windows/open_test_runner.bat
@@ -2,7 +2,7 @@
 
 cd %CUCUMBER_REPO_PATH%
 
-set URL="https://app.circleci.com/pipelines/github/ministryofjustice/testing-circle-ci?branch="
+set URL="https://app.circleci.com/pipelines/github/ministryofjustice/check-financial-eligibility?branch="
 for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set CURRENT_BRANCH=%%i
 
 git push
