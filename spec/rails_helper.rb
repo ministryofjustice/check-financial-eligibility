32b5e6cd008a6d9afff48c194cb8f755ea829dfd
Connor McQuillan <me@cpjmcquillan.com>
2022-11-29 07:04:15 +0000

    Prefer `Rails.root.join` when defining fixtures path

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = Rails.root.join("spec/fixtures")
 
   # Path to look up fixtures using `file_fixture('file_name.blah').read`.
   # Default is "spec/fixtures/files"
